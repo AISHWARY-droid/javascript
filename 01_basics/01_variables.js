4d1d5f91ab3d198a1e5063f3344891a05432d092
Aishwary Sharma <56380263+AISHWARY-droid@users.noreply.github.com>
2024-11-23 15:20:23 +0000

    conatiner config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,22 @@
+const accountId = 144553
+let accountEmail = "asharma@1234"
+var accountPassword = "12345"
+accountCity = "Raipur"
+let accountState
+
+//accountId = 2 //not allowed
+
+accountEmail = "asharma@3456"
+
+accountPassword = "2232425"
+
+accountCity = "Bilaspur"
+
+console.log(accountId)
+
+/*
+prefer not to use var 
+because of block scope and funtional scope
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
